200836be0fee275760f19829fd9477c3dd292397
unknown <edwinrivera1810@gmail.com>
2024-06-03 17:42:35 -0500

    cambios

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ function toggleMenu() {
         icon.innerHTML = '&#9776;';
     }
 }
+
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const slideWidth = slides[0].clientWidth;
+    const slideContainer = document.querySelector('.carousel-slide');
+    slideContainer.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % totalSlides;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+    showSlide(slideIndex);
+}
+
+function autoSlide() {
+    nextSlide();
+    setTimeout(autoSlide, 10000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+    setTimeout(autoSlide, 10000);
+});
